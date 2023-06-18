c75502c6842cacf30a1e3a91d075e428246b3986
Uday kalse <udaykalse123@gmail.com>
2023-06-19 00:34:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const apiKey = "10160c2098dc4174a9d183036231806"; // Replace with your Weather API key
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
+
+  const getWeatherDetails = (cityName) => {
+    if (!cityName) return;
+    const apiURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`;
+    axios
+      .get(apiURL)
+      .then((res) => {
+        console.log("response", res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value);
+    setInputCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    getWeatherDetails(inputCity);
+  };
+
+  return (
+    <div className="col-md-12">
+      <div className="wetherBg">
+        <h1 className="heading">Weather App</h1>
+        <div className="d-grid gap-3 col-4 mt-4">
+          <input
+            type="text"
+            className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {Object.keys(data).length > 0 && (
+        <div className="col-md-12 text-center mt-5">
+          <div className="shadow rounded wetherResultBox">
+            <img
+              className="weathorIcon"
+              src={"https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"}
+              alt="Weather Icon"
+            />
+            <h5 className="weathorCity">{data?.location?.name}</h5>
+            <h6 className="weathorTemp">{data?.current?.temp_c}°C</h6>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
